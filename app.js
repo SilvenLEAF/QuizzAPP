9e9d57597336e14a718f7482c14f1911c9bd8b02
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-13 08:43:58 +0530

    Server Listening with Express

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const path = require('path');
+const io = require('socket.io')(3000);
 
-const io = require('socket.io')(3000)
+
+
+// ----------------------FIRING EXPRESS APP
+const app = express();
+app.use(express.static(path.join(__dirname, 'client')));
+
+
 
 const users = {}
 
@@ -15,4 +24,42 @@ io.on('connection', socket => {
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
+})
+
+
+
+
+// ---------------------------CATCH-ALL HANDLER
+app.get('*', (req, res, next)=>{
+  try {
+    res.sendFile(path.join(__dirname, 'client/index.html'));
+  } catch (err) {
+    next(err, req, res);
+  }
+})
+
+
+
+
+
+
+
+// ---------------------------ERROR HANDLER
+app.use((err, req, res, next)=>{
+  console.log(err.message);
+  console.log(err);
+
+  res.json({ msg: `Server error`, error: err.message });
+})
+
+
+
+
+
+
+
+// ---------------------------LISTEN
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+  console.log(`Server is running on port ${ PORT }`);
 })
