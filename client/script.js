45340098e017339d3246ebd01ae23831fbd5d592
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-19 12:03:48 +0530

    refactored

--- client/script.js
+++ client/script.js
@@ -1,24 +1,58 @@
-const socket = io('http://localhost:5000')
+// Location of the SocketIO server
+const socket = io('http://localhost:5000'); 
+
+
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
+
+
+
+
+// Get NAME and emit NEW USER event
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
+
+
+
+
+
+
+/* -----------------------------------
+.       Handle SOCKET IO events
+----------------------------------- */
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
 })
 
+
+
+
+
 socket.on('user-connected', name => {
   appendMessage(`${name} connected`)
 })
+
+
+
 
 socket.on('user-disconnected', name => {
   appendMessage(`${name} disconnected`)
 })
 
+
+
+
+
+
+
+
+/* -----------------------------------
+.        MSG Form Socket IO event
+----------------------------------- */
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
   const message = messageInput.value
@@ -27,6 +61,19 @@ messageForm.addEventListener('submit', e => {
   messageInput.value = ''
 })
 
+
+
+
+
+
+
+
+
+
+
+/* -----------------------------------
+.   (Helper Function) DOM renderer
+----------------------------------- */
 function appendMessage(message) {
   const messageElement = document.createElement('div')
   messageElement.innerText = message
